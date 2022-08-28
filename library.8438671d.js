cb42e3d6015b58c1a2f1ae56f3cda05c1ce6ebe5
masiuk-mykola <masiuk.mykola@gmail.com>
2022-08-28 19:23:33 +0000

    Deploying to gh-pages from @ masiuk-mykola/team-project-filmoteka@5f8319334fbcda624e0b07fa6c274ca6625e6eb7 🚀

---
+++ library.8438671d.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>500||document.documentElement.scrollTop>500?document.getElementById("backToTop").style.display="block":document.getElementById("backToTop").style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=library.8438671d.js.map
