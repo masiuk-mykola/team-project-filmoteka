b2be12fa0d8c16e04d974ff247bcdd6502ffaef3
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-08-30 22:04:03 +0300

    Add pagination

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,14 @@
 import getMoviesApi from './getMoviesApi';
 import { appendMoviesMarkup } from './moviesMarkup';
 import { clearGallery } from './moviesMarkup';
+import { clearPagContainer } from './pagination';
+import { createPagination } from './pagination';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { loadFromTop } from './startPageGalleryRender';
 
 const searchMoviesApi = new getMoviesApi();
 const searchForm = document.querySelector('#search-form');
+const pagContainer = document.querySelector('.pag-container');
 
 searchForm.addEventListener('submit', onSearchFormSubmit);
 
@@ -12,7 +16,7 @@ export function onSearchFormSubmit(evt) {
   evt.preventDefault();
 
   searchMoviesApi.query = evt.currentTarget.elements.searchQuery.value;
-  const query = searchMoviesApi.query.trim();
+  const query = searchMoviesApi.searchQuery.trim();
 
   if (query === '') {
     return Notify.failure(
@@ -27,6 +31,7 @@ export function onSearchFormSubmit(evt) {
   searchMoviesApi.resetPage();
   searchMoviesApi.getSearchMovies().then(data => {
     const movies = data.results;
+
     clearGallery();
 
     if (movies.length === 0) {
@@ -36,6 +41,8 @@ export function onSearchFormSubmit(evt) {
     }
 
     appendMoviesMarkup(movies);
+    clearPagContainer();
+    createPagination(data.total_pages, 5);
     if (movies.length === 1) {
       return Notify.success('😮 We have found only one film for your request.');
     }
@@ -44,4 +51,45 @@ export function onSearchFormSubmit(evt) {
       `🎉 Hooray! We have found "${data.total_results}" films with "${query}".🥳`
     );
   });
+
+  pagContainer.addEventListener('click', paginate);
+
+  function paginate(evt) {
+    if (parseInt(evt.target.id) === searchMoviesApi.page) return;
+    if (evt.target.classList.contains('pag-btn')) {
+      searchMoviesApi.page = parseInt(evt.target.id);
+      clearGallery();
+      loadFromTop();
+      searchMoviesApi.getSearchMovies().then(movies => {
+        appendMoviesMarkup(movies.results);
+      });
+    }
+    if (
+      evt.target.classList.contains('move-left') ||
+      evt.target.classList.contains('move-right')
+    ) {
+      searchMoviesApi.page = parseInt(evt.target.id);
+      clearGallery();
+      loadFromTop();
+      searchMoviesApi.getSearchMovies().then(movies => {
+        appendMoviesMarkup(movies.results);
+      });
+    }
+    if (evt.target.classList.contains('to-start')) {
+      searchMoviesApi.page = 1;
+      clearGallery();
+      loadFromTop();
+      searchMoviesApi.getSearchMovies().then(movies => {
+        appendMoviesMarkup(movies.results);
+      });
+    }
+    if (evt.target.classList.contains('to-end')) {
+      searchMoviesApi.page = parseInt(evt.target.id);
+      clearGallery();
+      loadFromTop();
+      searchMoviesApi.getSearchMovies().then(movies => {
+        appendMoviesMarkup(movies.results);
+      });
+    }
+  }
 }
