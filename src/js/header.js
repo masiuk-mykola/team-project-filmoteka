cac7aba401fa7461ccb4fd3fb19d07d10d1e1d0a
Oleh-Lavreniuk <oleh4380211@gmail.com>
2022-08-31 03:32:16 +0300

    fix hurts

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,19 @@
-import { getAuth, onAuthStateChanged, } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Notiflix from 'notiflix';
 
 const hrefMyLibraryEl = document.querySelector('#my-library');
 const auth = getAuth();
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-        hrefMyLibraryEl.setAttribute('href', './library.html');
-        hrefMyLibraryEl.classList.remove('lib');
-    } 
-    else {
-        hrefMyLibraryEl.addEventListener('click', onClickhrefMyLibrary);  
-    }
-  });
-
-  function onClickhrefMyLibrary(){
-    Notiflix.Notify.info("Please register or sing in.");
+onAuthStateChanged(auth, user => {
+  if (user) {
+    const uid = user.uid;
+    hrefMyLibraryEl.setAttribute('href', './library.html');
+    hrefMyLibraryEl.classList.remove('lib');
+  } else {
+    hrefMyLibraryEl.addEventListener('click', onClickhrefMyLibrary);
   }
+});
+
+function onClickhrefMyLibrary() {
+  Notiflix.Notify.info('Please register or sing in.');
+}
