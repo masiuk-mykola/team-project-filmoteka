dcfb4559daf87f5b26037ec361f70137d3fb6da6
uliasuprun <uliasuprun88@gmail.com>
2022-08-31 16:38:59 +0300

    fixed button back-to-top

--- src/js/backToTop.js
+++ src/js/backToTop.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
         document.getElementById("backToTop").style.display = "block";
     } else {
         document.getElementById("backToTop").style.display = "none";
